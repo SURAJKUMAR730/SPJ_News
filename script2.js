037e27f5ef33d5030b7f49a9d7d99dab0487eb34
SURAJ KUMAR <106768289+SURAJKUMAR730@users.noreply.github.com>
2023-07-24 13:40:08 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,27 @@
+
+let header = document.querySelector('.header')
+let toggleMenu = document.querySelector('.bar')
+let menu = document.querySelector('nav ul')
+
+const toggle = (e) => {
+    toggleMenu.classList.toggle('active')
+    menu.classList.toggle('activeMenu')
+}
+
+toggleMenu.addEventListener('click', toggle)
+
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('sticky')
+    }
+    else {
+        header.classList.remove('sticky')
+    }
+})
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const apiKey = "dbc00603fe5c55fda86488151a6d83a7";
 
